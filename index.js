1d4048e89c1760c236761212ee5ec1570de89ad3
Matt <matt_thatch@outlook.com>
2023-02-16 10:53:14 +1030

    First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+    cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/test.html");
+});
+
+app.listen(5000)
+console.log('SERVER RUNNING ON PORT 5000');
+
+
+
+
+
+
+
+
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT);
