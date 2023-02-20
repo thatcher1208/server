8ce96c9be931cb674a99da4a7e711818358e40d4
Matt <matt_thatch@outlook.com>
2023-02-20 16:09:39 +1030

    second commit monday 20th

--- services/passport.js
+++ services/passport.js
@@ -20,20 +20,18 @@ passport.use
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     }, 
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-          if (existingUser) {
-            //already have recored with the profile ID
-            done(null, existingUser);
-          } else {
-            //we dont have a have a user with this ID, make new record
-            new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-          }
-      });
-    }
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
+    } 
   )
 );
