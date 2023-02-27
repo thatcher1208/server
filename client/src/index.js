e15977550e5839230787eea78fe7aefa5ecaf7be
Matt <matt_thatch@outlook.com>
2023-02-27 15:02:35 +1030

    commited

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,14 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-
+import axios from 'axios';
+window.axios = axios;
 const store = createStore (reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -23,20 +24,20 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/test.html");
-});
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(5000)
 console.log('SERVER RUNNING ON PORT 5000');
-
-
-
-
-
-
-
-
-//const PORT = process.env.PORT || 5000;
-//app.listen(PORT);
